6adb65ff78a0ab86aaeee9cb203524f78b6424a5
Maxim Humeniuk <goto.hmd@gmail.com>
2021-05-10 22:41:55 +0300

    initial

---
+++ test/test_task_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns test-task-clj.core-test
+  (:require [clojure.test :refer :all]
+            [test-task-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
